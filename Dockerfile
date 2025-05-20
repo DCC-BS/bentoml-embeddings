196cc66c9164817716c8f1761ca05a2a31be69d5
Yanick Schraner <yanick.schraner@bs.ch>
2025-05-20 13:08:32 +0200

    Update dependencies, improve docker fix issue with reranker

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,16 @@
-FROM nvidia/cuda:12.9.0-cudnn-runtime-ubuntu22.04
+FROM nvidia/cuda:12.4.1-cudnn-runtime-ubuntu22.04
 
 COPY --from=ghcr.io/astral-sh/uv:0.7.6 /uv /uvx /bin/
 
 RUN apt-get update && \
-    apt-get install -y git build-essential
+    apt-get install -y git build-essential && \
+    rm -rf /var/lib/apt/lists/* && \
+    apt-get clean
 
-ADD . /app
+COPY . /app
 WORKDIR /app
 
-RUN uv sync
-RUN MAX_JOBS=4 FLASH_ATTENTION_SKIP_CUDA_BUILD=TRUE uv pip install flash-attn==2.7.2.post1 --no-build-isolation
+RUN uv sync && \
+    MAX_JOBS=4 FLASH_ATTENTION_SKIP_CUDA_BUILD=TRUE uv pip install flash-attn==2.7.2.post1 --no-build-isolation
 
-ENTRYPOINT uv run bentoml serve service:Embeddings -p 50001
+ENTRYPOINT ["uv", "run", "bentoml", "serve", "service:Embeddings", "-p", "50001"]
--- service.py
+++ service.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
+
 import os
-import typing as t
-
-import numpy as np
-import bentoml
-from pydantic import Field
-from annotated_types import MinLen, MaxLen
 from typing import Annotated
-from torch import Tensor
 
+import bentoml
+import numpy as np
+from annotated_types import MaxLen, MinLen
+from pydantic import Field
+from rerankers.results import RankedResults
+from torch import Tensor
 
 EMBEDDING_MODEL_ID = os.getenv("EMBEDDING_MODEL_ID", "jinaai/jina-embeddings-v3")
 MAX_SEQ_LENGTH = int(os.getenv("MAX_SEQ_LENGTH", 8192))
@@ -21,7 +21,7 @@ LANGUAGE = os.getenv("LANGUAGE", "multi")
 EXAMPLE_INPUT = ["Ich esse gerne Pizza."]
 
 ValidString = Annotated[str, MinLen(1)]
-ValidStringList = Annotated[t.List[ValidString], MinLen(1), MaxLen(MAX_DOCS)]
+ValidStringList = Annotated[list[ValidString], MinLen(1), MaxLen(MAX_DOCS)]
 
 
 @bentoml.service(
@@ -30,10 +30,10 @@ ValidStringList = Annotated[t.List[ValidString], MinLen(1), MaxLen(MAX_DOCS)]
 class Embeddings:
     """
     BentoML service class for handling embeddings using the JinaAI jina-embeddings-v3 model.
-    
+
     This class defines a service that uses GPU resources and has a timeout of 60 seconds.
     The service is designed to process text inputs and generate corresponding embeddings.
-    
+
     Attributes:
         EMBEDDING_MODEL_ID (str): The identifier for the embedding model used by this service.
         RANKER_TYPE (str): Specifies the type of ranker used, in this case 'cross-encoder'.
@@ -43,22 +43,18 @@ class Embeddings:
 
     def __init__(self) -> None:
         import torch
-        from sentence_transformers import SentenceTransformer
         from rerankers import Reranker
+        from sentence_transformers import SentenceTransformer
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.encoder_model = SentenceTransformer(
-            EMBEDDING_MODEL_ID,
-            trust_remote_code=True,
-            device=self.device
+            EMBEDDING_MODEL_ID, trust_remote_code=True, device=self.device
         )
 
         # control your input sequence length up to 8192
         self.encoder_model.max_seq_length = MAX_SEQ_LENGTH
 
         self.ranker = Reranker(RANKER_TYPE, lang=LANGUAGE)
-
-
 
     @bentoml.api()
     def encode_documents(
@@ -76,12 +72,9 @@ class Embeddings:
         """
         task = "retrieval.passage"
         return self.encoder_model.encode(documents, task=task, prompt_name=task)
-    
+
     @bentoml.api()
-    def encode_queries(
-        self,
-        queries: ValidStringList
-    ) -> np.ndarray:
+    def encode_queries(self, queries: ValidStringList) -> np.ndarray:
         """
         Encodes a list of query strings into query embeddings.
 
@@ -93,14 +86,17 @@ class Embeddings:
         """
         task = "retrieval.query"
         return self.encoder_model.encode(queries, task=task, prompt_name=task)
-    
 
     @bentoml.api()
     def rerank(
         self,
-        documents: ValidStringList = Field(description="List of documents to be reranked"),
-        query: ValidString = Field(description="Query string used for ranking documents"),
-    ) -> t.Dict[int, t.Tuple[str,float,int]]:
+        documents: ValidStringList = Field(
+            description="List of documents to be reranked"
+        ),
+        query: ValidString = Field(
+            description="Query string used for ranking documents"
+        ),
+    ) -> dict[int, tuple[str, float, int]]:
         """
         Reranks a list of documents based on their relevance to a given query.
 
@@ -112,17 +108,25 @@ class Embeddings:
             Dict[int, Tuple[str, float, int]]: A dictionary mapping original indices
                 to tuples containing the document text, relevance score, and rank index.
         """
-        ranked_results = self.ranker.rank(query=query, docs=documents, doc_ids=list(range(0,len(documents))))
-        if len(documents) == 1:
-            result_json = {ranked_results.document.doc_id: (ranked_results.document.text, ranked_results.score, 0)}
-        else:
-            result_json = {result.doc_id: (result.text, result.score, result.rank) for result in ranked_results}
+        ranked_results: RankedResults = self.ranker.rank(
+            query=query, docs=documents, doc_ids=list(range(0, len(documents)))
+        )
+        result_json = {
+            result.doc_id: (
+                result.text,
+                result.score,
+                result.rank if result.rank is not None else 0,
+            )
+            for result in ranked_results
+        }
         return result_json
 
     @bentoml.api
     def tokenize(
         self,
-        documents: ValidStringList = Field(description="Documents that should be tokenized")
+        documents: ValidStringList = Field(
+            description="Documents that should be tokenized"
+        ),
     ) -> t.List[t.List[int]]:
         tokens: Tensor = self.encoder_model.tokenize(texts=documents)["input_ids"]
         return tokens.tolist()
